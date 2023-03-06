6e2a4e8efadd96d5e9feba75b65405be23f4eb7e
miki <milkyas.dev@gmail.com>
2023-03-06 21:07:37 +0300

    enhanced axios bug

--- next.config.js
+++ next.config.js
@@ -9,6 +9,6 @@ module.exports = {
     domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
   },
   env: {
-    rapidApi_key: '295d2a03bemsh028c4896ca2bc2dp16ddfajsn81d638536bd8'
+    api_key: '295d2a03bemsh028c4896ca2bc2dp16ddfajsn81d638536bd8'
   }
 }
--- utils/fetchAPi.js
+++ utils/fetchAPi.js
@@ -6,7 +6,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '295d2a03bemsh028c4896ca2bc2dp16ddfajsn81d638536bd8',
+            'X-RapidAPI-Key': process.env.api_key,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     })
